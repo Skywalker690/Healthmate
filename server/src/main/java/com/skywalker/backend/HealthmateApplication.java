1685572a395fe7a3d4ba86fcd86e32f1196d0fb4
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-18 21:15:52 +0530

    Initial commit

---
+++ server/src/main/java/com/skywalker/backend/HealthmateApplication.java
@@ -0,0 +1,13 @@
+package com.skywalker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthmateApplication.class, args);
+	}
+
+}
