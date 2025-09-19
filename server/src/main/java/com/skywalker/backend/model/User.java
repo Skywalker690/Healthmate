443fad70337cc6077e4c535685c39f426345e58c
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-19 21:16:33 +0530

    User Entity Done

--- server/src/main/java/com/skywalker/backend/model/User.java
+++ server/src/main/java/com/skywalker/backend/model/User.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.skywalker.backend.domain.USER_ROLE;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
@@ -23,33 +25,30 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name Required")
+    @NotBlank(message = "Name is required")
     private String name;
 
-    @Column(unique = true)
-    @NotBlank(message = "Email Required")
+    @Column(unique = true, nullable = false)
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
 
-    @NotBlank(message = "Password Required")
-    @JsonIgnore
+    @NotBlank(message = "Password is required")
+    @JsonIgnore // to not expose passwords in API
     private String password;
 
-    @NotBlank(message = "Phone Number Required")
     private String phoneNumber;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private USER_ROLE role = USER_ROLE.ROLE_PATIENT; // default role is PATIENT
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     @Column(updatable = false)
-    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime createdDate;
 
     @UpdateTimestamp
-    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
-    private LocalDateTime updateDate;
-
-    @Enumerated(EnumType.STRING)
-    private USER_ROLE role = USER_ROLE.ROLE_PATIENT;
-
-    @PrePersist
-    protected void onCreate() {
-        createdDate = LocalDateTime.now();
-    }
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    private LocalDateTime updatedDate;
 }
