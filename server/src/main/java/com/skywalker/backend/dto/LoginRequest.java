559ac1f7040857f0d77241f11c479bcb81a8e767
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-22 19:50:47 +0530

    LoginRequest

--- server/src/main/java/com/skywalker/backend/dto/LoginRequest.java
+++ server/src/main/java/com/skywalker/backend/dto/LoginRequest.java
@@ -1,4 +1,14 @@
 package com.skywalker.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+
+    @NotBlank(message = "Email Required ")
+    private String email;
+
+    @NotBlank(message = "Password Required ")
+    private String password;
 }
