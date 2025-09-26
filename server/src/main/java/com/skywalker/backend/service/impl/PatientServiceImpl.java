d2358761d0f42e62adda965118787ce9b13ebcd1
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-26 20:43:09 +0530

    PatientService

---
+++ server/src/main/java/com/skywalker/backend/service/impl/PatientServiceImpl.java
@@ -0,0 +1,39 @@
+package com.skywalker.backend.service.impl;
+
+import com.skywalker.backend.model.Patient;
+import com.skywalker.backend.repository.PatientRepository;
+import com.skywalker.backend.service.repo.PatientService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientServiceImpl(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Override
+    public Patient createPatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    @Override
+    public Optional<Patient> getPatientById(Long id) {
+        return patientRepository.findById(id);
+    }
+
+    @Override
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    @Override
+    public void deletePatient(Long id) {
+        patientRepository.deleteById(id);
+    }
+}
