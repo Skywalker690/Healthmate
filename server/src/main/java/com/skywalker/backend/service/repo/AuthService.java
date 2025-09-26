c8f37c25c7e5d2075d14be9292da3996feeb2186
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-26 21:06:46 +0530

    Refactor

--- server/src/main/java/com/skywalker/backend/service/repo/AuthService.java
+++ server/src/main/java/com/skywalker/backend/service/repo/AuthService.java
@@ -1,15 +1,15 @@
 package com.skywalker.backend.service.repo;
 
-import com.skywalker.backend.dto.LoginDTO;
-import com.skywalker.backend.dto.Response;
+import com.skywalker.backend.dto.LoginRequest;
+import com.skywalker.backend.dto.LoginResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface AuthService {
 
-    String registerUser(Response dto);
+    String registerUser(LoginResponse dto);
 
-    String loginUser(LoginDTO dto);
+    String loginUser(LoginRequest dto);
 
     boolean validateToken(String token);
 }
