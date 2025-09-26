a7c0bab7596f44c91ff26d818e15d515ecbcecdb
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-26 21:07:37 +0530

    Auth Controller

---
+++ server/src/main/java/com/skywalker/backend/controller/AuthController.java
@@ -0,0 +1,58 @@
+package com.skywalker.backend.controller;
+
+import com.skywalker.backend.dto.LoginRequest;
+import com.skywalker.backend.dto.LoginResponse;
+import com.skywalker.backend.dto.RegisterRequest;
+import com.skywalker.backend.model.User;
+import com.skywalker.backend.security.JwtTokenProvider;
+import com.skywalker.backend.service.repo.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(AuthenticationManager authenticationManager,
+                          JwtTokenProvider jwtTokenProvider,
+                          UserService userService,
+                          PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    // Register User
+    @PostMapping("/register")
+    public User registerUser(@RequestBody RegisterRequest registerRequest) {
+        User user = new User();
+        user.setName(registerRequest.getName());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setRole(registerRequest.getRole());
+        return userService.createUser(user);
+    }
+
+    // Login User
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        String token = jwtTokenProvider.generateToken(authentication);
+        return new LoginResponse(token, "Login successful!",true);
+    }
+}
