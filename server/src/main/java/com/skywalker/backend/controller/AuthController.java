417decc2a37ca57e952a8bf8db82c7ebd3ded273
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-27 16:16:59 +0530

    Role Based

--- server/src/main/java/com/skywalker/backend/controller/AuthController.java
+++ server/src/main/java/com/skywalker/backend/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.skywalker.backend.controller;
 
+import com.skywalker.backend.domain.USER_ROLE;
 import com.skywalker.backend.dto.LoginRequest;
 import com.skywalker.backend.dto.LoginResponse;
 import com.skywalker.backend.dto.RegisterRequest;
+import com.skywalker.backend.model.Doctor;
+import com.skywalker.backend.model.Patient;
 import com.skywalker.backend.model.User;
 import com.skywalker.backend.security.JwtTokenProvider;
+import com.skywalker.backend.service.repo.DoctorService;
+import com.skywalker.backend.service.repo.PatientService;
 import com.skywalker.backend.service.repo.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,10 +23,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManager authenticationManager;
+
+    private final UserService userService;
+    private final DoctorService doctorService;
+    private final PatientService patientService;
 
     // Register User
     @PostMapping("/register")
@@ -29,11 +37,25 @@ public class AuthController {
         User user = new User();
         user.setName(registerRequest.getName());
         user.setEmail(registerRequest.getEmail());
-        user.setPhoneNumber(registerRequest.getPhoneNumber());
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setRole(registerRequest.getRole());
-        return userService.createUser(user);
+
+        User savedUser = userService.createUser(user);
+
+        if (savedUser.getRole() == USER_ROLE.ROLE_PATIENT) {
+            Patient patient = new Patient();
+            patient.setUser(savedUser);
+            patientService.createPatient(patient);
+        }
+        else if (savedUser.getRole() == USER_ROLE.ROLE_DOCTOR) {
+            Doctor doctor = new Doctor();
+            doctor.setUser(savedUser);
+            doctorService.createDoctor(doctor);
+        }
+
+        return savedUser;
     }
+
 
     // Login User
     @PostMapping("/login")
