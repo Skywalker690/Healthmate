2c2f3189862bea1f18dae9241dd1173a1308f9bb
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-27 17:59:59 +0530

    Annotated

--- server/src/main/java/com/skywalker/backend/repository/AppointmentRepository.java
+++ server/src/main/java/com/skywalker/backend/repository/AppointmentRepository.java
@@ -2,9 +2,11 @@ package com.skywalker.backend.repository;
 
 import com.skywalker.backend.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AppointmentRepository  extends JpaRepository<Appointment,Long> {
     List<Appointment> findByPatientId(Long patientId);
 
--- server/src/main/java/com/skywalker/backend/repository/UserRepository.java
+++ server/src/main/java/com/skywalker/backend/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.skywalker.backend.repository;
 
 import com.skywalker.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    boolean existsByEmail(String email);
 
+    boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
 }
