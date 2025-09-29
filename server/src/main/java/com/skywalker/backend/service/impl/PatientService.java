fa0bb203e6864f318b1c3838d6468538ccc2554e
Skywalker690 <sanjoksiby0@gmail.com>
2025-09-29 15:41:09 +0530

    Test endpoints for patient

--- server/src/main/java/com/skywalker/backend/service/impl/PatientService.java
+++ server/src/main/java/com/skywalker/backend/service/impl/PatientService.java
@@ -4,19 +4,21 @@ import com.skywalker.backend.dto.Response;
 import com.skywalker.backend.exception.OurException;
 import com.skywalker.backend.model.Patient;
 import com.skywalker.backend.repository.PatientRepository;
+import com.skywalker.backend.repository.UserRepository;
 import com.skywalker.backend.security.Utils;
 import com.skywalker.backend.service.repo.IPatientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PatientService implements IPatientService {
 
     private final PatientRepository patientRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Response getAllPatients() {
@@ -52,8 +54,31 @@ public class PatientService implements IPatientService {
 
     }
 
+    @Transactional
     @Override
-    public void deletePatient(Long id) {
-        patientRepository.deleteById(id);
+    public Response deletePatient(Long id) {
+        Response response = new Response();
+        try {
+            Patient patient = patientRepository.findById(id)
+                    .orElseThrow(() -> new OurException("Patient not found, deletion failed"));
+
+
+            // Delete associated user and patient
+            Long userId = patient.getUser().getId();
+            patientRepository.deleteById(patient.getId());
+            userRepository.deleteById(userId);
+
+            response.setStatusCode(200);
+            response.setMessage("Patient deleted successfully");
+
+        } catch (OurException e) {
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error occurred while deleting patient: " + e.getMessage());
+        }
+        return response;
     }
+
 }
--- server/src/main/java/com/skywalker/backend/service/repo/IPatientService.java
+++ server/src/main/java/com/skywalker/backend/service/repo/IPatientService.java
@@ -1,12 +1,14 @@
 package com.skywalker.backend.service.repo;
 
 import com.skywalker.backend.dto.Response;
-import com.skywalker.backend.model.Patient;
 
-import java.util.Optional;
 
 public interface IPatientService {
-    Response getPatientById(Long id);
+
     Response getAllPatients();
-    void deletePatient(Long id);
+
+    Response getPatientById(Long id);
+
+    Response deletePatient(Long id);
+
 }
